e37112a0bf58be1b55fdf7992ec6cb9d60e5edc0
MRD112-lab <lp8afa@gmail.com>
2025-03-19 22:24:19 +0100

    Add files via upload

--- platek.cpp
+++ platek.cpp
@@ -6,53 +6,48 @@ using namespace std;
 
 
 int main() {
+    
+    random_device rd;  // Urządzenie losujące (może być różne na różnych komputerach)
+    mt19937 gen(rd()); // Generator Mersenne Twister
+    uniform_int_distribution<int> dist(0, 11);
+    
 	int bizila;
 	string text1, text2, text3;
 	int i, j, k;
 	string uwu[] = {"xoooo", "oxooo", "ooxoo", "oooxo", "oooox", "oooxo", "ooxoo", "oxooo"};
-	
-	
-	
-	string platki[] = {"*oooooooo", "o*ooooooo", "oo*oooooo", "ooo*ooooo", "oooo*oooo", "ooooo*ooo", "oooooo*oo", "ooooooo*o", "oooooooo*"};
+			
+	string platki[] = {"*ooooooooooo", "o*oooooooooo", "oo*ooooooooo", "ooo*oooooooo", "oooo*ooooooo", 
+	"ooooo*oooooo", "oooooo*ooooo", "ooooooo*oooo", "oooooooo*ooo",
+	    "ooooooooo*oo", "oooooooooo*o", "ooooooooooo*" };
 	
 	string platek = "oooo*oooo";
 	string pusta =  "ooooooooo";
+	
+	string tablica[] = {"oooooooooooo", "oooooooooooo", "oooooooooooo", "oooooooooooo", "oooooooooooo", 
+	"oooooooooooo", "oooooooooooo", "oooooooooooo", "oooooooooooo"};
+	
 
-	text1="dwa stare uchole ";
-	text2="lubia programowac";
-	text3 = text1+text2;
-	random_device rd;  // Urządzenie losujące (może być różne na różnych komputerach)
-    mt19937 gen(rd()); // Generator Mersenne Twister
-    uniform_int_distribution<int> dist(0, 8);
 
-	cin >>  bizila;
-	cout << "paweł to gawron"  << endl;
-	cout << bizila  << endl;
 
-	if (bizila > 42 ){
-		cout << "ivi lopa" << endl;
-	}
 
-	cout << text3  << endl;
-
-    for(k=0; k<=10; k++){
-        int losowa = dist(gen); 
-        string platek = platki[losowa];
-        for(j=0; j<=7; j++){
+        
+        for(j=0; j<=20; j++){
             std::cout << "\033[2J\033[H"; 
-            for(i=0; i<=7; i++){
-        	    
-        	    if(i==j)
-        	        cout<<platek<<endl;
-        	    else
-        	        cout<<pusta<<endl;
-        		//cout<<uwu[i]<<endl;
+            for(i=0; i<=8; i++){
+        	    cout<<tablica[i]<<endl;
         		//this_thread::sleep_for(chrono::seconds(1));
             }
             this_thread::sleep_for(chrono::milliseconds(700));
+            for(i=8; i>=1; i--){
+                tablica[i]=tablica[i-1];
+            }
+            int losowa = dist(gen); 
+            string platek = platki[losowa];
+            tablica[0]=platek;
+            
         }
-        this_thread::sleep_for(chrono::milliseconds(500));
-    }
+        //this_thread::sleep_for(chrono::milliseconds(500));
+
 
 return 0;
 }
